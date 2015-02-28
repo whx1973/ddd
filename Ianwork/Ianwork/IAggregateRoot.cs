db0be8cddcf2942f10868111859c62ad538b5f08
whx <whx1973@hotmail.com>
2015-02-28 14:14:55 +0800

    创建DDD项目

---
+++ Ianwork/Ianwork/IAggregateRoot.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ianwork
+{
+	/// <summary>
+	/// 表示实现此接口的类为聚合跟类
+	/// </summary>
+	public interface IAggregateRoot:IEntity
+	{
+	}
+}
